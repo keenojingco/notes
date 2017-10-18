764e39d9cadbc10703f4dc9471814a98becbb458
Enrique Jingco <enriquejingco@gmail.com>
2017-10-18 19:55:29 +0100

    adding CRUD routes adding fillable data

--- app/Notes.php
+++ app/Notes.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notes extends Model
 {
+    protected $fillable = ['user_id', 'title', 'note'];
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
